1fd51b3e8e2501c51b54aef4281dda1034e36587
Helter <heltherv@gmail.com>
2023-06-12 10:29:35 -0500

    point validation

--- Js/app.js
+++ Js/app.js
@@ -1,8 +1,13 @@
 import { validateInputs } from "./validation.js";
+import { api } from "../services/api.js";
 //Selecciona los inputs email y password
 const inputs = document.querySelectorAll(".login__input");
-
-
 inputs.forEach((input) => {
-    input.addEventListener('blur', (input) => validateInputs(input.target))
+    input.addEventListener('blur', (e) => validateInputs(e.target))
+})
+
+//Section contactenos
+const inputContactenos = document.querySelectorAll(".input_");
+inputContactenos.forEach((input) => {
+    input.addEventListener('blur', (e) => validateInputs(e.target))
 })
--- Js/validation.js
+++ Js/validation.js
@@ -8,33 +8,56 @@ export const validateInputs = (input) => {
     }
     
 }
-
 //objeto de inputs
 const typeInputs = {
     email: (input) => validate(input),
     password: (input) => validate(input),
-    
+    nombre: (input) => validate(input),
+    mensaje: (input) => validate(input),
 }
-    
+
 
 //funcion que valida si esta vacio y el formato del email
 const validate = (input) => {
     const value = input.value;
     const type = input.dataset.tipo;
-    // console.log(type);
-    if(value === ''){
-        console.log("la entrada no puede estar vacio, ni en blanco");
-    }else{
-        if(type === 'email')
-        {
-            const regex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
-            if(regex.test(value)){
-                console.log("es valido");
-            }else{
-                console.log("formato no valido");
-            }
+    if(value !== ''){
+        switch(type){
+            case "email": validaEmail(value);
+            break;
+            case "nombre": validaName(value);
+            break;
+            case "mensaje": validaMensaje(value);
+            break;
         }
+    }else{
+       console.log("la entrada no puede estar vacio, ni en blanco");
     }
 }
 
-    
+
+
+//funciones de validaciones inputs y textarea
+const validaEmail = (value) => {
+    const regex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+    if(regex.test(value)){
+        // console.log("valido");
+   }else{
+    //    console.log("no valido");
+   }
+}
+const validaName = (value) => {
+    if(value.length > 0 && value.length <= 40){
+        // console.log("valido");
+    }else{
+        // console.log("invalid");
+    }
+}
+
+const validaMensaje = (value) => {
+    if(value.length > 0 && value.length <= 120){
+        // console.log("valido");
+    }else{
+        // console.log("Invalid");
+    }
+}
