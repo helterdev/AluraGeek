b40c453576afec823301217b76a826eb15f78399
Helter <heltherv@gmail.com>
2023-07-26 16:02:03 -0500

    guardar los datos de producto en formato JSON

--- controller/sendProducts.js
+++ controller/sendProducts.js
@@ -2,18 +2,22 @@ import { api } from "../services/api.js";
 
 const formProducts = document.querySelector("#products");
 formProducts.addEventListener("submit", (e) => {
-    console.log(e);
     e.preventDefault();
     sendProducts(formProducts);
     
 });
 
+
 const sendProducts = (form) => {
     const newFormProducts = new FormData(form);
+    const img = newFormProducts.get("files").name;
+    newFormProducts.append('files', img)
     const dataObjectForm = Object.fromEntries(newFormProducts);
     api.sendFormProducts(dataObjectForm).then((response) => {
         if(!response.ok){
             throw new Error("Bad server response")
+        }else{
+            window.location.href = "../pages/send.html"
         }
     })
 }
